04cb78c9535941fc328360ee6d476724e5c54da9
Gilda <gilda@insignia4u.com>
2015-03-20 18:23:43 -0300

    Adds style to website.

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   resources :followings, only: [:create]
 
+  get '/about', to: 'pages#about'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
